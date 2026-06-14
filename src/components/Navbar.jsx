361f6d3997a632eff0dc7df92b06761c4970fbd8
parasonicc <yadavparas.28py@gmail.com>
2026-06-14 16:16:34 +0100

    upd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,8 +47,7 @@ export default function Navbar() {
               rel="noreferrer"
               className={styles.linkExternal}
             >
-              LinkedIn ↗
-            </a>
+              LinkedIn            </a>
           </li>
         </ul>
 
